53e05e1190c3b9b363574d7b9d166767eca830a3
Vadim <vdmkrchkn@gmail.com>
2020-06-26 13:55:51 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Currencies view</header>
+      <footer className="App-footer">
+        <a
+          className="App-link"
+          href="https://github.com/vdmkrchkn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See my repos at github
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
