2607475929d6c5d3a27d2f85798a6e014abd377c
Vadim <vdmkrchkn@gmail.com>
2020-07-20 11:13:02 +0300

    transform currencyView component from class to func with hooks

--- src/components/CurrencyView/CurrencyView.tsx
+++ src/components/CurrencyView/CurrencyView.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
 import Table from 'components/Table';
 import Chart from 'components/Chart';
 
@@ -15,77 +14,59 @@ interface IState {
   data: any;
 }
 
-/**
- * Component for rendering currency rates data.
- */
-class CurrencyView extends React.Component<IProps, IState> {
-  timerID!: NodeJS.Timeout;
-  /**
-   * @constructor
-   * @param {any} props - component's props.
-   */
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      data: {},
-    };
-  }
+export default ({currencies}: IProps) => {
+  const [state, setState] = useState<IState>({
+    data: {},
+  });
 
-  // eslint-disable-next-line require-jsdoc
-  componentDidMount() {
-    const {currencies} = this.props;
+  useEffect(() => {
     // request historical rates 4 testing
     if (process.env.NODE_ENV !== 'production') {
       getCurrencyRates(currencies, process.env.REACT_APP_FETCH_FROM)
-          .then(data => this.setState({data}))
+          .then(data => setState({data}))
           .catch(error => console.error(error.message));
     }
 
-    this.timerID = setInterval(
+    const {REACT_APP_FETCH_INTERVAL} = process.env;
+    const timerID = setInterval(
         () => {
           getCurrencyRates(currencies)
-              .then(data => {
-                if (!(Object.keys(data)[0] in this.state.data)) {
-                  this.setState(state => ({...state, data}));
+              .then(rates => {
+                if (!(Object.keys(rates)[0] in state.data)) {
+                  setState(prevState => {
+                    const {data} = prevState;
+
+                    return {'data': {...data, ...rates}};
+                  });
                 }
               })
               .catch(error => console.error(error.message));
         },
-        process.env.REACT_APP_FETCH_INTERVAL ? parseInt(process.env.REACT_APP_FETCH_INTERVAL) : 10000,
+        REACT_APP_FETCH_INTERVAL ? parseInt(REACT_APP_FETCH_INTERVAL) : 10000,
     );
+
+    return () => clearInterval(timerID);
+  }, [currencies]);
+
+  const dates = Object.keys(state.data);
+
+  if (dates.length > 0) {
+    const fields = ['date', ...currencies];
+
+    const renderData = dates.map(key => {
+      return {
+        [fields[0]]: key,
+        ...state.data[key],
+      };
+    });
+
+    return (
+      <div id="container">
+        <Table fields={fields} data={[...renderData].reverse()} maxRows={5} />
+        <Chart xAxisDataKey={fields[0]} data={renderData} />
+      </div>
+    );
+  } else {
+    return (<div>No data. Please wait for updates...</div>);
   }
-
-  // eslint-disable-next-line require-jsdoc
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  // eslint-disable-next-line require-jsdoc
-  render() {
-    const {data} = this.state;
-    const dates = Object.keys(data);
-
-    if (dates.length > 0) {
-      const {currencies} = this.props;
-      const fields = ['date', ...currencies];
-
-      const renderData = dates.map(key => {
-        return {
-          [fields[0]]: key,
-          ...data[key],
-        };
-      });
-
-      return (
-        <div id="container">
-          <Table fields={fields} data={[...renderData].reverse()} maxRows={5} />
-          <Chart xAxisDataKey={fields[0]} data={renderData} />
-        </div>
-      );
-    } else {
-      return (<div>No data. Please wait for updates...</div>);
-    }
-  }
-}
-
-export default CurrencyView;
+};
