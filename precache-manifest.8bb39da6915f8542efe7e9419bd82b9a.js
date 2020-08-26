4fe0ea2b8e8a18937b4bdccb704933c750d90144
Vadim Kurochkin <vadim_k@softgrad.lan>
2020-08-26 16:45:33 +0300

    Updates

---
+++ precache-manifest.8bb39da6915f8542efe7e9419bd82b9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37d3f6726effed21491610fb37ea3d3",
+    "url": "/currency-view/index.html"
+  },
+  {
+    "revision": "d0820558cd14b3e61123",
+    "url": "/currency-view/static/css/main.d61e84b1.chunk.css"
+  },
+  {
+    "revision": "1dfe6fd2e21470b8cbe6",
+    "url": "/currency-view/static/js/2.9e014fbb.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/currency-view/static/js/2.9e014fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0820558cd14b3e61123",
+    "url": "/currency-view/static/js/main.5d6e24e7.chunk.js"
+  },
+  {
+    "revision": "4b4f29783400a457069e",
+    "url": "/currency-view/static/js/runtime-main.62dc1a2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/currency-view/static/media/logo.5d5d9eef.svg"
+  }
+]);
